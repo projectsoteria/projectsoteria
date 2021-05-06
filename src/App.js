1ab347e845965d659fe2a20e287f7fbb0a5c8ff8
shreyravi01 <shreyravi01@gmail.com>
2021-05-06 17:59:22 -0400

    perf(graphql): moves request out of App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
-import * as queries from "./graphql/queries";
-
 import { API, graphqlOperation } from "aws-amplify";
-import Amplify, { Auth } from "aws-amplify";
-import React, { useEffect, useState } from "react";
-import {listArticles, listPosts} from "./graphql/queries";
+import React, {useState} from "react";
 
 import About from "./components/About";
 import ArticleContainer from "./components/ArticleContainer";
@@ -16,69 +12,22 @@ import PostContainer from "./components/PostContainer";
 import Row from "react-bootstrap/Row";
 import UserContextProvider from "./components/UserContextProvider";
 import UserPage from "./components/UserPage.js";
-import { withAuthenticator } from "@aws-amplify/ui-react";
 
 // TODO finish this
 function App() {
-  useEffect(() => {
-    API.graphql(
-      graphqlOperation(listArticles)
-    ).then((response) => {
-      console.log(response);
-      setArticles(response.data.listArticles.items);
-    });
-    API.graphql(
-      graphqlOperation(`
-      query ListPosts(
-        $filter: ModelPostFilterInput
-        $limit: Int
-        $nextToken: String
-      ) {
-        listPosts(filter: $filter, limit: $limit, nextToken: $nextToken) {
-          items {
-            userID
-            id
-            title
-            text
-            image
-            comments {
-              nextToken
-              items {
-                id
-                text
-              }
-            }
-            createdAt
-            updatedAt
-          }
-          nextToken
-        }
-      }
-    `)
-    ).then((response) => {
-      console.log(response);
-      setPosts(response.data.listPosts.items);
-    });
-  }, []);
-
   const [eventKey, setEventKey] = useState("Home");
-  ///const allTodos = await API.graphql({ query: queries.listTodos });
-  ///console.log(allTodos);
-  const [articles, setArticles] = useState([]);
-  const [posts, setPosts] = useState([]);
   let page = undefined;
   if (eventKey === "Home") {
     page = <Home></Home>;
   } else if (eventKey === "About") {
     page = <About></About>;
   } else if (eventKey === "Articles") {
-    page = <ArticleContainer articleData={articles}></ArticleContainer>;
+    page = <ArticleContainer></ArticleContainer>;
   } else if (eventKey === "User") {
     page = <UserPage></UserPage>;
   } else if (eventKey === "Posts") {
     page = (
       <PostContainer
-        postData={posts}
         onCreatePost={(newPost) =>
           API.graphql(
             graphqlOperation(
